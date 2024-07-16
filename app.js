62f62e90446f471515665f349c9ec3276e57c069
Augusto Bruno <menezesaugustobruno@gmail.com>
2024-07-15 21:51:22 -0300

    Adicionar novas informacoes
    Co-authored-by Augusto Bruno <menezesaugustobruno@gmail.com>

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ let chute = prompt('Escolha um número entre 1 e 10');
 if (chute == numeroSecreto) {
     console.log('Isso ai! Você descobriu o número secreto (5)');
 }
+//Fazendo alteração que o desafio mandou fazer
+
+alert('Augusto você é brabo!');
